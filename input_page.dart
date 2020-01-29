18c51ad42b2157c65bbe1f67e7b11b3244c29bcd
alceil <ashishshajisrampickal@gmail.com>
2020-01-29 23:41:17 +0530

    gy

--- input_page.dart
+++ input_page.dart
@@ -1,11 +1,15 @@
 import 'material.dart'
 import 'input_page.dart'
+const bottomcontainerheight=80.0;
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
 }
 
 class _InputPageState extends State<InputPage> {
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,6 +22,14 @@ class _InputPageState extends State<InputPage> {
           expanded(child:null),
           expanded(child:null),
         ]
+          container(
+          color:colors.red,
+        margin:edgeInsets.only(top:10.0),
+      width:double.infinity,
+      height:80.0,
+
+
+
       )
       ),
       floatingActionButton: FloatingActionButton(
