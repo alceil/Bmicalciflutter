ef8de23f4531c99409f256304d7eb7d8abae2137
alceil <ashishshajisrampickal@gmail.com>
2020-01-29 22:43:17 +0530

    added background color

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,12 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme:Themedata.dark.copywith(
+        primarycolor:colors.purple,
+        ScaffoldBackgroundcolor:color.red
+        accentcolor:colors.red,
+        texttheme:Texttheme(body1:TextStyle(color:Colors.white))
+      )
       home: InputPage(),
     );
   }
