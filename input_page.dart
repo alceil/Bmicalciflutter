6396028852c6f45e8c3cf537104df99a4c6540fc
alceil <ashishshajisrampickal@gmail.com>
2020-01-29 22:45:55 +0530

    yo

---
+++ input_page.dart
@@ -0,0 +1,23 @@
+import 'material.dart'
+import 'input_page.dart'
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI CALCULATOR'),
+      ),
+      body: Center(
+        child: Text('Body Text'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
