5cef7240c00c0ad9580796756f2f2ecaa3a0ce20
alceil <ashishshajisrampickal@gmail.com>
2020-01-30 11:12:55 +0530

    gy

--- input_page.dart
+++ input_page.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/cupertino.dart';
+
 import 'material.dart'
 import 'input_page.dart'
 const bottomcontainerheight=80.0;
@@ -35,6 +37,22 @@ class _InputPageState extends State<InputPage> {
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
       ),
+    );
+  }
+}
+class ReusableCard extends StatelessWidget{
+  ReusableCard({@required this.colour,this.cardChild});
+  final Color colour;
+  final Widget cardChild;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child:cardChild ,
+      margin:EdgeInsets.all(15.0),
+      decoration:BoxDecoration(
+        color:colour,
+        borderRadius: BorderRadius.circular(10.0),
+      )
     );
   }
 }
